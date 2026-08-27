58d91895fc218b1e97cee4c33562c972a99ce580
Mohamed Asath <asath12882@gmail.com>
2026-08-27 08:23:06 +0530

    Serve basemap tiles from OpenStreetMap instead of CARTO
    CARTO's keyless basemap tiles are fair-use and intermittently blocked,
    which left the hosted maps blank. OSM's standard tile server needs no
    API key.

--- apps/kaduna-web/src/components/LeafletMap.tsx
+++ apps/kaduna-web/src/components/LeafletMap.tsx
@@ -28,8 +28,8 @@ export default function LeafletMap({ incidents, hotspots, blackspots, onSelectIn
 
       L.control.zoom({ position: "topright" }).addTo(map);
 
-      L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
-        attribution: '&copy; <a href="https://carto.com/">CARTO</a>',
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         maxZoom: 19,
       }).addTo(map);
 
